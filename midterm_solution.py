0e97ce26a1e5fe489d92863758079439bb484cae
unknown <alyssazeanne.lumanlan@fcpc.edu.ph>
2026-04-18 11:22:26 +0800

    midterm_solution.py

---
+++ midterm_solution.py
@@ -0,0 +1,17 @@
+
+str(input("Student name: "))
+int(input("Weekly budget: "))
+
+print("=" * 35)
+print("WEEKLY EXPENSE -- CATEGORIES")
+print("=" * 35)
+
+expense_category = [
+    ["1. Food & Drinks", "[e.g. Lunch, snacks, coffee]"],
+    ["2. Transportation", "[e.g. Bus, jeepney, ride-share]"],
+    ["3. Mobile / Internet", "[e.g. Load, data plan, WiFi top-up]"],
+    ["4. School", "[e.g. Notebook, pen, bond paper]"],
+    ["5. Entertainment", "[e.g. Games, movies, hangout]"],
+]
+
+print(expense_category)
